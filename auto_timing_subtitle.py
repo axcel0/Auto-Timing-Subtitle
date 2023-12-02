f7941a3a3c246c251eddee15d9150dca749460e1
Axel Santoso <41705027+axcel0@users.noreply.github.com>
2023-12-03 02:11:03 +0700

    first commit

---
+++ auto_timing_subtitle.py
@@ -0,0 +1,124 @@
+from enum import auto
+from tkinter import *
+from tkinter import filedialog
+from process import auto_sub_jp
+
+def callback():
+    global process
+    global done
+    if process is not None:
+        process.destroy()
+    process = Label(window,text="procssing.....")
+    process.pack()
+    
+    type_ = value_type.get()
+    model = value_model.get()
+    split = value_split.get()
+    method = value_method.get()
+    beam = beam_size.get(1.0, "end-1c")
+    file_name = filename
+    
+    split_.destroy()
+    method_.destroy()
+    model_menu.destroy()
+    model_attention.destroy()
+    button_process.destroy()
+    type_menu.destroy()
+    split_menu.destroy()
+    method_menu.destroy()
+    beam_size.destroy()
+    beam_.destroy()
+    auto_sub_jp(type_, model, split, method, beam, file_name)
+    if done is not None:
+        done.destroy()
+    done = Label(window, text="Done!")
+    done.pack()
+def browseFiles():
+    global filename
+    global path_
+    filename = filedialog.askopenfilename(initialdir="/",
+                                          title="Select a File",
+                                          filetypes=(("MP4 Files", "*.mp4*"),("All Types", "*.*")))
+    if path_ is not None:
+        path_.destroy()
+    path_ = Label(window, text=f"Selected files:\n{filename}", width=100, height=5, bg='white')
+    path_.pack()
+    required_list()
+    advanced_settings()
+    list_model()
+    
+
+def list_model():
+    global model_menu
+    global model_attention
+    global button_process
+    global value_model
+    model_list = ["large-v2", "large-v3"]
+    model_attention = Label(window,text="Model size will affect the processing time and transcribe quality\nmodel large-v2 is from faster-whisper\nmodel large-v3 is from whisper")
+    model_attention.pack()
+    value_model = StringVar(window)
+    value_model.set("Select Model")
+    model_menu = OptionMenu(window, value_model, *model_list)
+    model_menu.pack()
+    button_process = Button(window, text="Click to Process",command=callback)
+    button_process.pack()
+
+def required_list():
+    global type_menu
+    global value_type
+    type_list = ["audio","video"]
+    value_type = StringVar(window)
+    value_type.set("Select uploaded file type")
+    type_menu = OptionMenu(window, value_type, *type_list)
+    type_menu.pack()
+def advanced_settings():
+    global split_
+    global method_
+    global split_menu
+    global value_method
+    global value_split
+    global method_menu
+    global beam_
+    global beam_size
+    if split_ is not None:
+        split_.destroy()
+    split_ = Label(window, text="Option for split line text by spaces. The splited lines all use the same time stamp, with 'adjust_required' label as remark for manual adjustment")
+    split_.pack
+    split_list = ["No","Yes"]
+    value_split=StringVar(window)
+    value_split.set("Use Split?")
+    split_menu = OptionMenu(window, value_split, *split_list)
+    split_menu.pack
+    
+    if method_ is not None:
+        method_.destroy()
+    method_ = Label(window, text="Normal segmentation (Modest): When the length of the text after the space exceeds 5 characters, start a new line\nSplit all (Aggressive): start a new line as soon as a space is encountered")
+    method_.pack()
+    split_method_list=["Modest","Aggressive"]
+    value_method = StringVar(window)
+    value_method.set("Select split method")
+    method_menu = OptionMenu(window, value_method, *split_method_list)
+    method_menu.pack()
+    
+    if beam_ is not None:
+        beam_.destroy()
+    beam_ = Label(window, text="The higher the Beam Size value, the more paths are explored during recognition, which can help improve recognition accuracy within a certain range,\nbut the relative VRAM usage will also be higher. At the same time, the Beam Size may decrease after exceeding 5-10.\nDefaut Beam size is 5")
+    beam_.pack()
+    beam_size = Text(window, height=1, width=20)
+    beam_size.pack()
+    
+window = Tk()
+window.title('Auto timing subtitle')
+window.geometry('800x600')
+window.config(background='white')
+
+path_ = None
+process = None
+split_ = None
+method_ = None
+beam_ = None
+done = None
+button_explore = Button(window, text="Browse Files", command=browseFiles)
+button_explore.pack()
+
+window.mainloop()
---
+++ process.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+import subprocess
+from tqdm import tqdm
+import time
+from urllib.parse import quote_plus
+import pysubs2
+import torch 
+import whisper
+from faster_whisper import WhisperModel
+from srt2ass import srt2ass
+
+def auto_sub_jp(file_type, model_size, is_split, split_method, beam_size, file_name):
+    language = 'ja'
+    sub_style = "default"
+    is_vad_filter = "False"
+    set_beam_size = int(beam_size)
+    file_basename = Path(file_name).stem
+    output_dir = Path(file_name).parent.resolve()
+    print('Loading model...')
+
+    if model_size == 'large-v3':
+        model = whisper.load_model("large-v3")
+        is_whisperv3 = True
+    else:
+        model = WhisperModel(model_size)
+        is_whisperv3 = False
+
+    #Transcribe
+    if file_type == "video":
+        print('Extracting audio from video file...')
+        ffmpeg_path = "./ffmpeg.exe"
+        ffmpeg_command = [ffmpeg_path, '-i', file_name, '-f', 'mp3', '-ab', '192000', '-vn', f'{file_basename}.mp3']
+        try:
+            subprocess.run(ffmpeg_command,check=True)
+        except Exception as e:
+            print(f"Error occurred while extracting audio: {e}")
+        print('Done.')
+    tic = time.time()
+    print('Transcribe in progress...')
+
+    if is_whisperv3:
+        results = model.transcribe(audio = f'{file_basename}.mp3', language= language, verbose=False)
+    else:
+        segments, info = model.transcribe(audio = f'{file_basename}.mp3',
+                                            beam_size=set_beam_size,
+                                            language=language,
+                                            vad_filter=is_vad_filter,
+                                            vad_parameters=dict(min_silence_duration_ms=1000))
+        total_duration = round(info.duration, 2)
+        results= []
+        with tqdm(total=total_duration, unit=" seconds") as pbar:
+            for s in segments:
+                segment_dict = {'start':s.start,'end':s.end,'text':s.text}
+                results.append(segment_dict)
+                segment_duration = s.end - s.start
+                pbar.update(segment_duration)
+
+    toc = time.time()
+    print('Done')
+    print(f'Time consumpution {toc-tic}s')
+    subs = pysubs2.load_from_whisper(results)
+    subs.save(f'{output_dir}/{file_basename}.srt')
+
+    ass_sub = srt2ass(f"{output_dir}/{file_basename}.srt", sub_style, is_split,split_method)
+    print(f'ASS subtitle saved as: {ass_sub}')
+
+    torch.cuda.empty_cache()
